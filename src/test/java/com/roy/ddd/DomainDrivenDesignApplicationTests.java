5664b614ea643dc04b707a501708d1e77ffc30bf
roy-zz <74298434+roy-zz@users.noreply.github.com>
2022-11-11 16:50:54 +0900

    Initial commit

---
+++ src/test/java/com/roy/ddd/DomainDrivenDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roy.ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainDrivenDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
